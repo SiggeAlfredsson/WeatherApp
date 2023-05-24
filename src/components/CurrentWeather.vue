0af70d7c01ab32e463388d98cefcfd24822607e2
Sigge <saorust@hotmail.com>
2023-05-25 01:54:27 +0200

    change padding

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -3,15 +3,17 @@
 
 <template>
   <div class="curr-weather-details">
-    <img :src="getWeatherIconUrl(currWeather.weather[0].icon)" alt="weather icon">
-    <h1>{{ currWeather.name }}, {{ currWeather.sys.country }}</h1>
+    <div class="icon-h1">
+      <img :src="getWeatherIconUrl(currWeather.weather[0].icon)" alt="weather icon">
+      <h1>{{ currWeather.name }}, {{ currWeather.sys.country }}</h1>
+    </div>
     <h2><span id="desc">Weather:</span> {{ currWeather.weather[0].main }} </h2>
     <h2><span id="desc">Temperature:</span> {{ Math.round(currWeather.main.temp) }}&deg</h2>
-    
+
     <h2><span id="desc">Humidity:</span> {{ currWeather.main.humidity }}%</h2>
     <h2>
-      <span id="desc">Wind:</span> {{ (currWeather.wind.speed*0.44704).toFixed(1) }} 
-      <span v-if="!isNaN(currWeather.wind.gust)">({{ (currWeather.wind.gust*0.44704).toFixed(1) }})</span> m/s
+      <span id="desc">Wind:</span> {{ (currWeather.wind.speed * 0.44704).toFixed(1) }}
+      <span v-if="!isNaN(currWeather.wind.gust)">({{ (currWeather.wind.gust * 0.44704).toFixed(1) }})</span> m/s
     </h2> <!-- M/s Är det ända rätta måttet på vind... -->
     <h2><span id="desc">Wind deg:</span> {{ currWeather.wind.deg }}&deg </h2>
     <h2><span id="desc">Pressure:</span> {{ currWeather.main.pressure }}</h2>
@@ -30,30 +32,32 @@ export default {
       required: true
     }
   },
-methods: {
-  getWeatherIconUrl(icon: string): string {
-    return `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  methods: {
+    getWeatherIconUrl(icon: string): string {
+      return `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    }
   }
-}
 }
 
 </script>
 
 <style>
-body {
-  padding: 30px;
-  /* border: red 1px solid; */
-
-}
 
 .curr-weather-details {
   /* border: red 1px solid; */
-  padding: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
   margin: 10px;
 
   background-color: rgb(0, 173, 181);
   border-radius: 30px;
   width: 65%;
+}
+
+/* gör icon och h1 brevid varandra */
+.icon-h1 {
+  display: flex;
+  align-items: center;
 }
 
 #desc {
